108d8d762ede9c84aaa9d4aa930272d325d4e8b7
Gord Stephen <gord.stephen@nrel.gov>
2023-02-16 12:46:47 -0700

    Update dummy datasource for new state data structures

--- datasource/dummy/main.go
+++ datasource/dummy/main.go
@@ -9,12 +9,40 @@ import (
 )
 
 type DummyDataSource struct {
+
     freq time.Duration
     lastTime time.Time
+
+    regions []internal.Region
+    categories []internal.UnitCategory
+    units []internal.UnitMetadata
+
 }
 
 func New(freq time.Duration) *DummyDataSource {
-    return &DummyDataSource { freq: freq }
+
+    regions := []internal.Region {
+        { "Region A" }, { "Region B" },
+    }
+
+    categories := []internal.UnitCategory {
+        { "C1", "#00FF00" }, { "C2", "#0000FF" },
+    }
+
+    units := []internal.UnitMetadata {
+        { "U1", &categories[0], &regions[0] },
+        { "U2", &categories[0], &regions[1] },
+        { "U3", &categories[1], &regions[0] },
+        { "U4", &categories[1], &regions[1] },
+    }
+
+    return &DummyDataSource {
+        freq: freq,
+        regions: regions,
+        categories: categories,
+        units: units,
+    }
+
 }
 
 func (d *DummyDataSource) Query() (internal.SystemState, error) {
@@ -29,22 +57,13 @@ func (d *DummyDataSource) Query() (internal.SystemState, error) {
     d.lastTime = now
 
     units := []internal.UnitState {
-
-        { internal.UnitMetadata { "U1", "C1", "R1"},
-          randBool(), 10, 100 },
-
-        { internal.UnitMetadata { "U2", "C1", "R2" },
-          randBool(), 5, 50 },
-
-        { internal.UnitMetadata { "U3", "C2", "R1" },
-          randBool(), 10, 100 },
-
-        { internal.UnitMetadata { "U4", "C2", "R2" },
-          randBool(), 1, 100 },
-
+        { d.units[0], randBool(), 10, 100 },
+        { d.units[1], randBool(), 5, 50 },
+        { d.units[2], randBool(), 10, 100 },
+        { d.units[3], randBool(), 1, 100 },
     }
     
-    return internal.SystemState { now, units }, nil
+    return internal.SystemState { now, 1500, units }, nil
 
 }
 
